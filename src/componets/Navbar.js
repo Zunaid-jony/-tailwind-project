fac79482f51d032e2858dd21d4a357fb6bfb4718
unknown <jony35666666@gmail.com>
2022-09-10 20:42:13 +0600

    navber menu

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HiMenu } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handelNev = () =>{
+        setNav(!nav)
+    }
     return (
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
           <h1 className='w-full text-3xl font-bold text-[#00df9a]'>  Navbar page</h1>
@@ -13,17 +18,18 @@ const Navbar = () => {
             <li className='p-4'>Contact</li>
 
           </ul>
-          <div>
-          <HiMenu size={20}></HiMenu>
+          <div onClick={handelNev}>
+            {!nav ? <AiOutlineClose size={20}/> : <HiMenu size={20}></HiMenu>}
+          
           </div>
           <div className='fixed left-0 top-0  h-full w-[60%] border-r border-r-gray-900 bg-gray-600'>
-          <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>  Navbar page</h1>
+          <h1 className='w-full text-3xl font-bold text-[#00df9a] m-5'>  Navbar page</h1>
           <ul className=' uppercase p-4'>
-            <li className='p-4 border-b'>Home</li>
-            <li className='p-4 border-b'>Company</li>
-            <li className='p-4 border-b'> Resources</li>
-            <li className='p-4 border-b'>About</li>
-            <li className='p-4 border-b'>Contact</li>
+            <li className='p-4 border-b border-gray-500'>Home</li>
+            <li className='p-4 border-b border-gray-500'>Company</li>
+            <li className='p-4 border-b border-gray-500'> Resources</li>
+            <li className='p-4 border-b border-gray-500'>About</li>
+            <li className='p-4 border-b border-gray-500'>Contact</li>
 
           </ul>
 
