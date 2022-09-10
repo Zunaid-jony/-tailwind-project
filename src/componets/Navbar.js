0bdd4e52a3505d0de4aea621adfa6f0e73788406
unknown <jony35666666@gmail.com>
2022-09-10 23:13:46 +0600

    navigation a

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <h1 className='w-full text-3xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-green-500 to-blue-600 hover:from-pink-500 hover:to-yellow-500'>TAILWIND </h1>
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
         <li className='p-4'>Company</li>
@@ -18,11 +18,12 @@ const Navbar = () => {
         <li className='p-4'>About</li>
         <li className='p-4'>Contact</li>
       </ul>
-      <div onClick={handleNav} className='block md:hidden'>
+      <div onClick={handleNav} className='block md:hidden'>  
+      {/* md:hidden dile midium hole menu icons dekhabe sma:dile khob choto hole menu dekahbe */}
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-5 mt-8'>REACT.</h1>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#292b29] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%] '}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-5 mt-8 font-bold bg-gradient-to-r text-transparent bg-clip-text from-green-500 to-blue-600 hover:from-pink-500 hover:to-yellow-500'>TAILWIND</h1>
           <li className='p-4 border-b border-gray-600'>Home</li>
           <li className='p-4 border-b border-gray-600'>Company</li>
           <li className='p-4 border-b border-gray-600'>Resources</li>
