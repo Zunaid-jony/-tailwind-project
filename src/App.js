62fb89a84aa935cfc09d87a8d05a4c8309ac3abd
unknown <jony35666666@gmail.com>
2022-09-11 00:41:16 +0600

    typing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Hero from "./componets/Hero";
 import Navbar from './componets/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
     
       <Navbar></Navbar>
+      <Hero></Hero>
       
     </div>
   );
---
+++ src/componets/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';import Typed from 'react-typed';
+const Hero = () => {
+    return (
+        <div className='text-white '>
+           <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+           <p className='text-[#00df93] font-bold p-2 uppercase '>hero section</p>
+            <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>hero section</h1>
+            <div className='flex justify-center items-center'>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, Flexible financing for</p>
+                {/* Typed ata live typing show kore dekah jai  */}
+                <Typed
+                className='md:text-5xl sm:text-4xl   text-xl font-bold pl-2'
+                 strings={['BOSS', 'BTC', 'SASS']}
+                 typeSpeed={120}
+                    backSpeed={140}
+                    loop 
+                
+                ></Typed>
+            </div>
+           </div>
+        </div>
+    );
+};
+
+export default Hero;
